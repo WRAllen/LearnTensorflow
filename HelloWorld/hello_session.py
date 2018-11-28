dc7906f61bc8a37fd2f9bbbce0839135d646f780
WRAllen <1072274105@qq.com>
2018-11-28 10:41:45 +0800

    一点补充

--- HelloWorld/hello_session.py
+++ HelloWorld/hello_session.py
@@ -1,7 +1,8 @@
 ###标准版
 import tensorflow as tf
-
+###申明一个常量
 tensor = tf.constant('Hello, world!')
+# print(tensor)
 with tf.Session() as sess:
     result = sess.run(tensor)
     print(result)
--- HelloWorld/hello_withoutsession.py
+++ HelloWorld/hello_withoutsession.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 import tensorflow.contrib.eager as tfe 
 ###引入了动态图机制Eager Execution
 tfe.enable_eager_execution() 
-
+###申明一个常量
 tensor = tf.constant('Hello, world!(no need session)')
+# print(tensor)
 tensor_value = tensor.numpy()
 print(tensor_value)
