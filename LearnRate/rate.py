7158d4e8ecebe687cd6beeebfeb40868ffca6185
WRAllen <1072274105@qq.com>
2018-12-26 11:39:30 +0800

    添加学习速率

--- LearnRate/rate.py
+++ LearnRate/rate.py
@@ -1,2 +1,13 @@
 # -*- coding:utf-8 -*-
+import tensorflow as tf 
+global_step = tf.Variable(10)
+#通过exponential_decay函数生成学习速率
+learning_rate = tf.train.exponential_decay(0.1, global_step, 10, 0.96, staircase=True)
+
+with tf.Session() as sess:
+	init = tf.global_variables_initializer()
+	sess.run(init)
+	print(sess.run(learning_rate))
+#输出
+# 0.096
 
