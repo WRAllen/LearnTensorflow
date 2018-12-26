b69130579a2477a8d3d839d0383b2aa52fffdc3a
WRAllen <1072274105@qq.com>
2018-12-26 17:40:26 +0800

    增加滑动平均

---
+++ ExponentialMovingAverage/move.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+import tensorflow as tf
+
+#定义一个变量用于计算滑动平均，这个变量的初始值设置为0，并且滑动平均的变量必须是实数型
+v1 = tf.Variable(0, dtype=tf.float32)
+#迭代轮数，不可训练trainable设置为false
+step = tf.Variable(0, trainable=False)
+#定义一个滑动平均的类．初始化的时候要给定衰减率(0.99,一般衰减率都要非常的接近1．)，和控制衰减率的变量step
+ema = tf.train.ExponentialMovingAverage(0.99, step)
+#这个列表中的变量都会被更新
+maintain_average_op = ema.apply([v1])
+with tf.Session() as sess:
+	init = tf.global_variables_initializer()
+	sess.run(init)
+	print("初始化")
+	print(sess.run([v1, ema.average(v1)]))
+	print("更新变量v1的值到5")
+	sess.run(tf.assign(v1, 5))
+	#更新了变量的值也会导致影子变量和衰减率的变化
+	#衰减率min{0.99, (1 + step)/(10 + step)}因为这里step = 0,所以衰减率decay = 0.1
+	#相应的滑动平均(影子变量)变为 0.1*0 + 0.9*5 = 4.5
+	sess.run(maintain_average_op)
+	print(sess.run([v1, ema.average(v1)]))
+	print("更新step和v1的值")
+	sess.run(tf.assign(step, 10000))
+	#更新v1的值
+	sess.run(tf.assign(v1, 10))
+	#更新了变量的值也会导致影子变量和衰减率的变化
+	#衰减率min{0.99, (1 + step)/(10 + step)}因为这里step = 10000,所以衰减率decay = 0.99
+	#相应的滑动平均(影子变量)变为 0.99*4.5 + 0.01*10 = 4.555
+	sess.run(maintain_average_op)
+	print(sess.run([v1, ema.average(v1)]))
+	print("再次更新滑动平均")
+	sess.run(maintain_average_op)
+	print(sess.run([v1, ema.average(v1)]))
+# 输出
+# 初始化
+# [0.0, 0.0]
+# 更新变量v1的值到5
+# [5.0, 4.5]
+# 更新step和v1的值
+# [10.0, 4.555]
+# 再次更新滑动平均
+# [10.0, 4.60945]
