902dca843e82bf1a9a23e59e3aa84b84c27c0bae
WRAllen <1072274105@qq.com>
2019-01-02 15:14:51 +0800

    移动

--- train_demo/high_demo/demo_1/demo.py
+++ train_demo/high_demo/demo_1/demo.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
 sess = tf.InteractiveSession()
 
 #创建权重的随机噪点
